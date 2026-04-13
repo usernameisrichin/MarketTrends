57b1ab2a61bad4b5579a8c0c9b33963b513fd451
usernameisrichin <richinthomas.mec@gmail.com>
2026-04-13 18:15:01 +0530

    chore(app): add Flutter-generated plugin registration and lock files

---
+++ app/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<local_auth_darwin/LocalAuthPlugin.h>)
+#import <local_auth_darwin/LocalAuthPlugin.h>
+#else
+@import local_auth_darwin;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [LocalAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"LocalAuthPlugin"]];
+}
+
+@end
